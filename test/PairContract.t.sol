6af001d204f918db0cb7a1e5ace89dd30ba43e17
Hamid Bateni <hamidmrd79@gmail.com>
2024-01-28 10:29:27 +0400

    Feat: add test spec for limit order

--- test/PairContract.t.sol
+++ test/PairContract.t.sol
@@ -36,7 +36,41 @@ contract PairTest is Test {
         // TODO
     }
     function test_InsertLimitOrder() public {
-        // TODO
+        /// @NOTE Scenario 1 => Insert a limit maker order in buy side.
+        /// Acceptance Criteria:
+        ///      - Token transferred to the contract with the correct amount.
+        ///      - Order get located in the orders Mapping with 0 orderID.
+        ///      - Order get placed in the first position of  pricePoint
+        ///      - PricePoint get updated correctly.
+        ///      - check orderID 0 struct to check all the values are correct.
+        ///      - check the event is emitted correctly.
+        ///      - in the last step we should check the orderID is incremented correctly.
+
+        /// @NOTE Scenario 2 => Insert another limit maker order in buy side.
+        /// Acceptance Criteria:
+        ///      - The same checks as scenario 1.
+
+        /// @NOTE Scenario 3 => Insert a limit maker order in sell side.
+        /// Acceptance Criteria:
+        ///      - The same checks as scenario 1.
+
+        /// @NOTE Scenario 4 => Insert another limit maker order in sell side.
+        /// Acceptance Criteria:
+        ///      - The same checks as scenario 1.
+
+        /// @NOTE Scenario 5 => Insert a limit taker order in buy side.
+        /// Acceptance Criteria:
+        ///      - The all checks in the scenario 1 should get passed.
+        ///      - user should receive the correct amount of baseToken for getting matched
+        ///      - the pricePoint should update correctly in both sides.
+
+        /// @NOTE Scenario 6 => Insert a limit taker order in sell side.
+        /// Acceptance Criteria:
+        ///      - The all checks in the scenario 1 should get passed.
+        ///      - user should receive the correct amount of quoteToken for getting matched
+        ///      - the pricePoint should update correctly in both sides.
+
+        
     }
 
     function test_MarketOrder() public {
