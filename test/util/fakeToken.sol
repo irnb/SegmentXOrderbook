b6c2b92cd6c6c5fc211b404d909a110f1caf49cf
Hamid Bateni <hamidmrd79@gmail.com>
2024-01-28 20:51:50 +0400

    Feat: add more customizability to the fake token contract

--- test/util/fakeToken.sol
+++ test/util/fakeToken.sol
@@ -1,12 +1,23 @@
 // SPDX-License-Identifier: TBD
 pragma solidity 0.8.20;
 
-import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract FakeToken is ERC20 {
-    constructor(string memory name_, string memory symbol_, address tokenReceiver)
-        ERC20(name_, symbol_)
-    {
-        _mint(tokenReceiver, 1000 ether);
+    uint8 decimal;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address tokenReceiver,
+        uint8 _decimal,
+        uint256 initialValue
+    ) ERC20(name_, symbol_) {
+        decimal = _decimal;
+        _mint(tokenReceiver, initialValue);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimal;
     }
 }
